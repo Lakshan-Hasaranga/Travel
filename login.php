f53ea61e47031a23c9aee5ae2c07e58df793f3be
Lakshan-Hasaranga <lakshanhasaranga23@gmail.com>
2026-09-04 18:57:22 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <style>
+        .container{
+            max-width: 1000px;
+            margin: auto;
+            box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+                transform: translateY(130px);
+        }
+        .container img{
+            width: 100%;
+            height: auto;
+        }
+        .row{
+            display: flex;
+        }
+        .col{
+            width: 50%;
+        }
+        .login-form-wrapper{
+             transform: translateY(100px);
+        }
+        .login-form-wrapper{
+            text-align: center;
+}
+       
+    </style>
+</head>
+<body>
+   <div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="./img/travel.jpg" alt="Beautiful women">
+        </div>
+         <div class="col">
+           <div class="login-form-wrapper">
+             <h2>Travel Login</h2>
+           </div>
+            
+        </div>
+    </div>
+   </div> 
+</body>
+</html>
